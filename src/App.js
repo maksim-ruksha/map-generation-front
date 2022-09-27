add3ab840810564078bd3e50768bd0913c859dfc
Maksim Ruksha <urpanium@gmail.com>
2022-09-27 19:09:03 +0300

    MP-17 make routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import LoginPage from "./components/LoginPage/LoginPage";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+
+import {LOGIN_PAGE_ROUTE} from "./routes/Routes";
+import {Fragment} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <Router>
+            <Fragment>
+                <Routes>
+                    <Route exact path={LOGIN_PAGE_ROUTE} element={<LoginPage/>}>
+                    </Route>
+                </Routes>
+            </Fragment>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/components/LoginPage/LoginPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import {Typography} from "@mui/material";
+
+export default function LoginPage() {
+    return (<Typography>
+        Jessie I am the Login Page
+    </Typography>);
+}
